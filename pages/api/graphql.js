5552d98a8a51811c5531158e386385b1b829b90e
Nick Foden <nicfoden@gmail.com>
2019-11-06 14:34:17 -0500

    Add graphql endpoint

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,17 +1,30 @@
-//Short way with Next.js req and res
-export default (req, res) => {
-  res.status(200).json({
-    test: "Hello Testing the endpoint"
-  });
+import {
+  AppolloServer,
+  gql,
+  ApolloError,
+  ApolloServer
+} from "apollo-server-micro";
+
+const typeDefs = gql`
+  type Query {
+    sayHello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHello: () => {
+      return "Hello Apollo Fans";
+    }
+  }
 };
 
-//Longer Way
-// export default (req, res) => {
-//   res.setHeader("Content-Type", "application/json");
-//   res.statusCode = 200;
-//   res.end(
-//     JSON.stringify({
-//       test: "Hello Testing the endpoint"
-//     })
-//   );
-// };
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
