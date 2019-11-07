b996d962266348ebfad2d6b751994c4392f83f6e
Nick Foden <nicfoden@gmail.com>
2019-11-07 10:22:01 -0500

    Remove unused styles and external links

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,6 @@
 import React from "react";
 import Link from "next/link";
 
-const links = [
-  { href: "https://zeit.co/now", label: "ZEIT" },
-  { href: "https://github.com/zeit/next.js", label: "GitHub" }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`;
-  return link;
-});
-
 const Nav = () => (
   <nav>
     <ul>
@@ -22,11 +14,6 @@ const Nav = () => (
           <a>About</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
     </ul>
 
     <style jsx>{`
--- pages/index.js
+++ pages/index.js
@@ -17,28 +17,7 @@ const Home = () => {
   return (
     <Layout>
       <div className="hero">
-        <h1 className="title">{data.sayHello}</h1>
-        <p className="description">
-          To get started, edit <code>pages/index.js</code> and save to reload.
-        </p>
-
-        <div className="row">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Learn more about Next.js in the documentation.</p>
-          </a>
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Next.js Learn &rarr;</h3>
-            <p>Learn about Next.js by following an interactive tutorial!</p>
-          </a>
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub.</p>
-          </a>
-        </div>
+        <h1 className="title">Track Dev Progress</h1>
       </div>
 
       <style jsx>{`
@@ -56,35 +35,6 @@ const Home = () => {
         .title,
         .description {
           text-align: center;
-        }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-around;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
-          text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
-        }
-        .card:hover {
-          border-color: #067df7;
-        }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
-        }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
         }
       `}</style>
     </Layout>
