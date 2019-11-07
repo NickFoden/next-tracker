bf57454c83c0f45f6331a7435765b2c91bdf9f08
Nick Foden <nicfoden@gmail.com>
2019-11-07 11:20:07 -0500

    Adding last 5 days with dates

--- components/Habit.js
+++ components/Habit.js
@@ -1,17 +1,26 @@
 import HabitButton from "./HabitButton";
 const Habit = ({ habit }) => {
+  const dates = getLast5Days();
+
   return (
     <article>
       <h3>{habit}</h3>
       <div>
-        <HabitButton />
-        <HabitButton />
-        <HabitButton />
-        <HabitButton />
-        <HabitButton />
+        {dates.map(date => (
+          <HabitButton key={date.getMilliseconds()} date={date} />
+        ))}
       </div>
     </article>
   );
+};
+
+const getLast5Days = () => {
+  const dates = "01234".split("").map(day => {
+    const tempDate = new Date();
+    tempDate.setDate(tempDate.getDate() - day);
+    return tempDate;
+  });
+  return dates;
 };
 
 export default Habit;
--- components/HabitButton.js
+++ components/HabitButton.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
-const HabitButton = () => {
+const HabitButton = ({ date }) => {
   const [complete, setComplete] = useState(false);
   return (
-    <button onClick={() => setComplete(!complete)}>
-      {" "}
-      {complete ? "X" : "O"}{" "}
-    </button>
+    <span>
+      {date.getMonth() + 1}/{date.getDate()}
+      <button onClick={() => setComplete(!complete)}>
+        {complete ? "X" : "O"}{" "}
+      </button>
+    </span>
   );
 };
 
