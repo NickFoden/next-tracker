04babc919f5f6ba17435c762c65586693c0bd71d
Nick Foden <nicfoden@gmail.com>
2019-11-07 10:30:25 -0500

    Add habit

---
+++ components/Habit.js
@@ -0,0 +1,16 @@
+const Habit = () => {
+  return (
+    <article>
+      <h3>Title</h3>
+      <div>
+        <button>O</button>
+        <button>O</button>
+        <button>O</button>
+        <button>X</button>
+        <button>O</button>
+        <button>X</button>
+        <button>O</button>
+      </div>
+    </article>
+  );
+};
