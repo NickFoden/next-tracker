44e2cf070dabf3a97257cd50827fce0b335e6585
Nick Foden <nicfoden@gmail.com>
2019-11-08 12:15:32 -0500

    Updating folders to use now

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,9 +1,9 @@
 import { gql, ApolloServer } from "apollo-server-micro";
 import { mergeResolvers, mergeTypeDefs } from "graphql-toolkit";
 import connectDb from "../../lib/mongoose";
-import { habitsResolvers } from "../../api/habits/resolvers";
-import { habitsMutations } from "../../api/habits/mutations";
-import Habits from "../../api/habits/Habits.graphql";
+import { habitsResolvers } from "../../src/api/habits/resolvers";
+import { habitsMutations } from "../../src/api/habits/mutations";
+import Habits from "../../src/api/habits/Habits.graphql";
 
 const fakeTypeDefs = gql`
   type Query {
